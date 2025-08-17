df04bf09a4c82c7a17503562eb95edca237550b2
idoumbouya <doumbouyaibrahima@gmail.com>
2025-08-17 04:18:03 +0000

    . Commandes et Requêtes

---
+++ gn-transport.Application/Commands/CreerDemandeTransportCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using gn_transport.Domain.ValueObjects;
+namespace gn_transport.Application.Commands
+{
+    public class CreerDemandeTransportCommand
+    {
+        public string TypeProduit { get; set; }
+        public double Quantite { get; set; }
+        public string UniteQuantite { get; set; }
+        public Localisation PointDepart { get; set; }
+        public Localisation PointArrivee { get; set; }
+        public DateTime DateHeureSouhaitee { get; set; }
+        public Guid DemandeurId { get; set; }
+    }
+
+}
---
+++ gn-transport.Application/Queries/GetDemandeTransportByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace gn_transport.Application.Queries
+{
+    public class GetDemandeTransportByIdQuery
+    {
+        public Guid DemandeId { get; set; }
+    }
+}
