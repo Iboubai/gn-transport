e533b16b3f78994baa3109cf82d54b1ddaa8e3f4
idoumbouya <doumbouyaibrahima@gmail.com>
2025-08-17 04:18:54 +0000

    Gestionnaires de Commandes et de Requêtes (Handlers)

---
+++ gn-transport.Application/Handlers/CreerDemandeTransportCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using gn_transport.Domain.Ports.Input;
+using gn_transport.Application.Commands;
+using gn_transport.Domain.Entities;
+namespace gn_transport.Application.Handlers
+{
+    public class CreerDemandeTransportCommandHandler
+    {
+        private readonly IGestionDemandeTransportService
+            _gestionDemandeTransportService;
+        public
+            CreerDemandeTransportCommandHandler(IGestionDemandeTransportService
+                gestionDemandeTransportService)
+        {
+            _gestionDemandeTransportService = gestionDemandeTransportService;
+        }
+        public async Task<DemandeTransport> Handle(CreerDemandeTransportCommand
+            command, CancellationToken cancellationToken)
+        {
+            return await
+                _gestionDemandeTransportService.CreerDemandeTransportAsync(
+                    command.TypeProduit,
+                    command.Quantite,
+                    command.UniteQuantite,
+                    command.PointDepart,
+                    command.PointArrivee,
+                    command.DateHeureSouhaitee,
+                    command.DemandeurId
+                );
+        }
+    }
+}
