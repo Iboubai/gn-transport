82becc208a4d29ff1d1cd788c79b7520545792e6
idoumbouya <doumbouyaibrahima@gmail.com>
2025-08-17 04:16:12 +0000

    Interfaces des Ports d'Entrée (Driving Ports)

---
+++ gn-transport.Domain/Ports/Input/IGestionDemandeTransportService.cs
@@ -0,0 +1,17 @@
+﻿using gn_transport.Domain.Entities;
+using gn_transport.Domain.ValueObjects;
+namespace gn_transport.Domain.Ports.Input
+{
+    public interface IGestionDemandeTransportService
+    {
+        Task<DemandeTransport> CreerDemandeTransportAsync(string typeProduit,
+            double quantite, string uniteQuantite, Localisation pointDepart, Localisation
+                pointArrivee, DateTime dateHeureSouhaitee, Guid demandeurId);
+        Task<DemandeTransport> GetDemandeTransportByIdAsync(Guid id);
+        Task<IEnumerable<DemandeTransport>>
+            GetDemandesTransportByDemandeurIdAsync(Guid demandeurId);
+        Task AnnulerDemandeTransportAsync(Guid id);
+        Task AccepterDemandeTransportAsync(Guid id);
+        // Autres méthodes pour la gestion des offres, matching, etc.
+    }
+}
